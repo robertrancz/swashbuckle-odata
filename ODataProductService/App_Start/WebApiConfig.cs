7f57159ff7f41846382238917a612e8fa25b24f0
robertrancz <rancz.robi@gmail.com>
2017-12-18 22:02:10 +0200

    Create the Products OData service.

---
+++ ODataProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using ODataProductService.Models;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace ODataProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+
+            // OData routes
+            config.MapODataServiceRoute(routeName: "ODataRoute", routePrefix: null, model: builder.GetEdmModel());
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+        }
+    }
+}
